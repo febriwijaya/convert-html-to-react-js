0e0a6271ad3ca1cb83bf6e761cf1b27aba913a56
Febri Wijaya <tampuboloncarles21@gmail.com>
2021-11-07 09:02:44 +0700

    First Init

---
+++ src/elements/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+import propTypes from "prop-types";
+
+export default function Button(props) {
+  const className = ["button"];
+  if (props.isPrimary) className.push("button-primary");
+  if (props.isWideMobile) className.push("button-wide-mobile");
+  if (props.isSmall) className.push("button-sm");
+  if (props.isBlock) className.push("button-block");
+
+  return (
+    <div>
+      <button className={className.join(" ")}>{props.children}</button>
+    </div>
+  );
+}
+
+Button.propTypes = {
+  isPrimary: propTypes.bool,
+  isWideMobile: propTypes.bool,
+  isSmall: propTypes.bool,
+  isBlock: propTypes.bool,
+};
---
+++ src/scenes/Homepage.js
@@ -0,0 +1,194 @@
+import React from "react";
+import Header from "part/Header";
+import Section from "elements/Section";
+import Button from "elements/Button";
+import HeroImage from "assets/images/hero-image.png";
+import Fade from "react-reveal/Fade";
+import Clients from "part/Clients";
+import Feature from "part/Feature";
+import List from "elements/Lists";
+import Card from "elements/Card";
+import FeatureTile01 from "assets/images/feature-tile-icon-01.svg";
+import FeatureTile02 from "assets/images/feature-tile-icon-02.svg";
+import FeatureTile03 from "assets/images/feature-tile-icon-03.svg";
+import Footer from "part/Footer";
+
+export default function Homepage() {
+  const features = [
+    {
+      imgSrc: FeatureTile01,
+      imgAlt: "Feature tile icon 01",
+      title: "Join the BWA",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile02,
+      imgAlt: "Feature tile icon 02",
+      title: "Join the BWA",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile03,
+      imgAlt: "Feature tile icon 03",
+      title: "Join the BWA",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    },
+  ];
+
+  const listPricing = [
+    {
+      price: 27,
+      currencySymbol: "$",
+      description: "Lorem ipsum is a common text",
+      features: [
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: false, content: "Excepteur sint occaecat velit" },
+        { isChecked: false, content: "Excepteur sint occaecat velit" },
+      ],
+    },
+    {
+      price: 47,
+      currencySymbol: "$",
+      description: "Lorem ipsum is a common text",
+      features: [
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: false, content: "Excepteur sint occaecat velit" },
+      ],
+    },
+    {
+      price: 67,
+      currencySymbol: "$",
+      description: "Lorem ipsum is a common text",
+      features: [
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+        { isChecked: true, content: "Excepteur sint occaecat velit" },
+      ],
+    },
+  ];
+
+  return (
+    <div className="body-wrap">
+      <Header></Header>
+      <main className="site-content">
+        <Section className="hero illustration-section-01" isCenteredContent>
+          <div className="container-sm">
+            <div className="hero-inner section-inner">
+              <div className="hero-content">
+                <Fade bottom delay={500}>
+                  <h1 className="mt-0 mb-16">Landing template for startups</h1>
+                </Fade>
+                <div className="container-xs">
+                  <Fade bottom delay={1000}>
+                    <p className="mt-0 mb-32">
+                      Our landing page template works on all devices, so you
+                      only have to set it up once, and get beautiful results
+                      forever.
+                    </p>
+                  </Fade>
+                </div>
+              </div>
+              <div
+                className="hero-figure illustration-element-01"
+                data-reveal-value="20px"
+                data-reveal-delay="800"
+              >
+                <Fade bottom delay={1500}>
+                  <img
+                    className="has-shadow"
+                    src={HeroImage}
+                    alt="Hero"
+                    width="896"
+                    height="504"
+                  />
+                </Fade>
+              </div>
+            </div>
+          </div>
+        </Section>
+        <Clients></Clients>
+        <Section className="features-tiles">
+          <div className="container">
+            <div className="features-tiles-inner section-inner">
+              <div className="tiles-wrap">
+                {features.map((feature, index) => (
+                  <Feature
+                    key={index}
+                    delayInMS={index * 500}
+                    data={feature}
+                  ></Feature>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Section>
+        <Section className="pricing">
+          <div className="container">
+            <div className="pricing-inner section-inner has-top-divider">
+              <div className="section-header center-content">
+                <div className="container-xs">
+                  <h2 className="mt-0 mb-16">Simple, transarent pricing</h2>
+                  <p className="m-0">
+                    Lorem ipsum is common placeholder text used to demonstrate
+                    the graphic elements of a document or visual presentation.
+                  </p>
+                </div>
+              </div>
+              <div className="tiles-wrap">
+                {listPricing.map((list, index) => (
+                  <Card hasShadow>
+                    <div class="pricing-item-content">
+                      <div class="pricing-item-header pb-24 mb-24">
+                        <div class="pricing-item-price mb-4">
+                          <span class="pricing-item-price-currency h2">
+                            {list.currencySymbol}
+                          </span>
+                          <span
+                            class="pricing-item-price-amount h1 pricing-switchable"
+                            data-pricing-monthly="74"
+                            data-pricing-yearly="67"
+                          >
+                            {list.price}
+                          </span>
+                        </div>
+                        <div class="text-xs text-color-low">
+                          {list.description}
+                        </div>
+                      </div>
+                      <div class="pricing-item-features mb-40">
+                        <div class="pricing-item-features-title h6 text-xs text-color-high mb-24">
+                          What’s included
+                          <List
+                            data={list.features}
+                            isSmall
+                            className="pricing-item-features-list mb-32"
+                          ></List>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="pricing-item-cta mb-8">
+                      <Button isPrimary isBlock>
+                        Start free trial
+                      </Button>
+                    </div>
+                  </Card>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Section>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
+}
